cec44c23d277ee332f52ce6d67cc786b1e9139d9
fthorde <manyids2@gmail.com>
2020-09-24 22:09:54 +0200

    making pip package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mkd_pytorch",
+    version="1.0.0",
+    author="Arun Mukundan",
+    author_email="arun.mukundan@gmail.com",
+    description="Multiple kernel local descriptors.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/manyids2/mkd_pytorch",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
