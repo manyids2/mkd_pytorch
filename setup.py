79c7edfbdeb0213076f828f5d966687a705a6a09
fthorde <manyids2@gmail.com>
2020-09-25 10:17:41 +0200

    bumped version to v1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mkd_pytorch",
-    version="1.0.0",
+    version="1.1.0",
     author="Arun Mukundan",
     author_email="arun.mukundan@gmail.com",
     description="Multiple kernel local descriptors.",
