7019a2e183ed899e37d3cb5b3720db10dff8abe3
fthorde <manyids2@gmail.com>
2020-09-25 10:24:01 +0200

    bump again, forgot to push.

--- mkd_pytorch/__init__.py
+++ mkd_pytorch/__init__.py
@@ -2,6 +2,6 @@ from .mkd import MKD
 from .layers import L2Norm, VonMisesKernel, Gradients, EmbedGradients, ExplicitSpacialEncoding
 from .kernel import COEFFS, get_grid, get_kron_order
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __author__ = 'Arun Mukundan'
 
